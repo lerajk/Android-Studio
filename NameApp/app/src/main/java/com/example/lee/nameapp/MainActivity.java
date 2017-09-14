f4508eabcbcb8a25e4a6919ea2b74cb7a806cb73
lerajk <lerajk44@gmail.com>
2017-09-14 15:08:57 -0400

    changes

--- NameApp/app/src/main/java/com/example/lee/nameapp/MainActivity.java
+++ NameApp/app/src/main/java/com/example/lee/nameapp/MainActivity.java
@@ -4,11 +4,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String Program = "Program";
+    public static final String Course = "Course";
+    public static final String Semester = "3";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,11 +21,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendMessage(View view){
 
-        String id = "N";
-
+        String id = "Name";
         Intent intent = new Intent(this, Main2Activity.class);
         String name = ((TextView) findViewById(R.id.textView)).getText().toString();
+
+        TextView program = (TextView) findViewById(R.id.Program);
+        TextView course = (TextView) findViewById(R.id.Course);
+        TextView semester = (TextView) findViewById(R.id.Semester);
+
+        String messageprogram = program.getText().toString();
+        String messagecourse = program.getText().toString();
+        String messagesemester = program.getText().toString();
+
         intent.putExtra(id, name);
+        intent.putExtra(Program,messageprogram);
+        intent.putExtra(Course, messagecourse);
+        intent.putExtra(Semester, messagesemester);
         startActivity(intent);
 
     }
