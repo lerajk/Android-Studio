32718da2537dce4ac99c07cbb5b0fbf5756ba5ec
lerajk <lerajk44@gmail.com>
2017-09-26 17:19:38 -0400

    progress

--- MyApplication/app/src/main/java/com/example/lee/leenoyrajkhowa_comp304lab2/activity_customerInfo.java
+++ MyApplication/app/src/main/java/com/example/lee/leenoyrajkhowa_comp304lab2/activity_customerInfo.java
@@ -1,7 +1,9 @@
 package com.example.lee.leenoyrajkhowa_comp304lab2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class activity_customerInfo extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class activity_customerInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_info);
+    }
+
+    public void back_button_two(View view)
+    {
+        Intent intent = new Intent(activity_customerInfo.this, activity_foodmenu.class);
+        startActivity(intent);
     }
 }
--- MyApplication/app/src/main/java/com/example/lee/leenoyrajkhowa_comp304lab2/activity_foodmenu.java
+++ MyApplication/app/src/main/java/com/example/lee/leenoyrajkhowa_comp304lab2/activity_foodmenu.java
@@ -1,7 +1,9 @@
 package com.example.lee.leenoyrajkhowa_comp304lab2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class activity_foodmenu extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class activity_foodmenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foodmenu);
+    }
+
+    public void back_button(View view)
+    {
+        Intent intent = new Intent(this, activity_cuisine.class);
+        startActivity(intent);
     }
 }
