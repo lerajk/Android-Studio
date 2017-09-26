fcb16284ea6a4ffbec8cec5937be9f598f5ed38f
lerajk <lerajk44@gmail.com>
2017-09-26 17:42:26 -0400

    progress

--- MyApplication/app/src/main/java/com/example/lee/leenoyrajkhowa_comp304lab2/activity_customerInfo.java
+++ MyApplication/app/src/main/java/com/example/lee/leenoyrajkhowa_comp304lab2/activity_customerInfo.java
@@ -4,13 +4,28 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class activity_customerInfo extends AppCompatActivity {
+
+    EditText nameValidation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_info);
+
+        nameValidation = (EditText) findViewById(R.id.Name);
+
+        nameValidation.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if(nameValidation.getText().length()<5){
+                    nameValidation.setError("Enter more than 5 characters");
+                }
+            }
+        });
     }
 
     public void back_button_two(View view)
